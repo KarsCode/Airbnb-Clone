e2f4a734210bd9d942848da3455fda4e5ccccb75
AnirveshArcot <arcotanirvesh@gmail.com>
2023-07-06 21:12:12 +0530

    footer

---
+++ client/src/Footer.js
@@ -0,0 +1,81 @@
+
+
+const Footer = () => {
+    return ( 
+        <div className="Footer">
+            <div className="footerDetails">
+                <section>
+                    <h1>Support</h1>
+                    <ul>
+                        <li><a href="#">Help Center</a></li>
+                        <li><a href="#">AirCover</a></li>
+                        <li><a href="#">Supporting people with disabilities</a></li>
+                        <li><a href="#">Cancellation options</a></li>
+                        <li><a href="#">Our COVID-19 Response</a></li>
+                        <li><a href="#">Report a neighbourhood concern</a></li>
+                    </ul>
+                </section>
+                    
+
+                <section>
+                    <h1>Community</h1>
+                    <ul>
+                        <li><a href="#">Airbnb.org: disaster relief housing</a></li>
+                        <li><a href="#">Combating discrimination</a></li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h1>Hosting</h1>
+                    <ul>
+                        <li><a href="#">Airbnb your home</a></li>
+                        <li><a href="#">AirCover for Hosts</a></li>
+                        <li><a href="#">Explore hosting resources</a></li>
+                        <li><a href="#">Visit our community forum</a></li>
+                        <li><a href="#">How to host responsibly</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h1>Airbnb</h1>
+                    <ul>
+                        <li><a href="#">Newsroom</a></li>
+                        <li><a href="#">Learn about new features</a></li>
+                        <li><a href="#">Letter from our founders</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Investors</a></li>
+                    </ul>
+                </section>
+            </div>
+            <div className="footerCopyright">
+                <div className="copyrightContent">
+                    <div className="footerLeft">
+                        <p>© 2023 Airbnb, Inc.</p>
+                        <ul>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Terms</a></li>
+                            <li><a href="#">Sitemap</a></li>
+                            <li><a href="#">Company details</a></li>
+
+                        </ul>
+                    </div>
+                    <div className="footerRight">
+                        <div className="footerCurrency">
+
+                        </div>
+                        <div className="footerSocials">
+                            <p>Socials</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+     );
+}
+ 
+export default Footer;
